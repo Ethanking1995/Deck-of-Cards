0b1e347de4e159160af8656b7bfbc565a704eca8
Ethan <ethanking1016@gmail.com>
2020-01-21 12:08:55 -0500

    App handles creating a new deck, shuffling deck, and drawing five cards

--- deckofcards.js
+++ deckofcards.js
@@ -1,9 +1,24 @@
 const fetch = require("node-fetch");
-
-let deck = fetch(
-  "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
-).then(response => {
-  response.json().then(data => {
-    console.log(data);
-  });
-});
+let deckId = null;
+let shuffled = false;
+//grab a shuffled deck
+fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1").then(
+  response => {
+    response.json().then(data => {
+      //record deck Id
+      deckId = data.deck_id;
+      shuffled = data.shuffled;
+      //grab five cards of that shuffled deck
+      fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=5`).then(
+        response => {
+          response.json().then(data => {
+            data.cards.forEach(card => {
+              //print their value and suit
+              console.log(card.value + " of " + card.suit);
+            });
+          });
+        }
+      );
+    });
+  }
+);
